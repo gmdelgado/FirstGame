34375511bc5cfac0efd213856a156d48ed5a231d
Gabriel Martinez Delgado <106568654+gmdelgado@users.noreply.github.com>
2023-04-27 19:32:21 -0600

    Update cameraControl script
    [SerializeField] int sensHor;
        [SerializeField] int sensVer;
    
        [SerializeField] int lockVertMin;
        [SerializeField] int lockVertMax;
    
        [SerializeField] bool invertY;
    
        float xRotation;
    
        // Start is called before the first frame update
        void Start()
        {
            Cursor.visible = false;
            // turn this off for testing game so you can move the cursor around when playing in unity
                // turn back on when publishing game
            Cursor.lockState = CursorLockMode.Locked;
        }
    
        // Update is called once per frame
        void Update()
        {
            //Get Input
            float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensVer;
            float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensHor;
            // Convert input to rotation float
            xRotation += mouseY;
            // clamp camera to rotation
            xRotation = Mathf.Clamp(xRotation, lockVertMin, lockVertMax);
    
            //rotate the camera on the X-Axis
            transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
    
            //rotate the player on the Y-Axis
            transform.parent.Rotate(Vector3.up * mouseX);
        }

--- First Game/Assets/Scripts/cameraControls.cs
+++ First Game/Assets/Scripts/cameraControls.cs
@@ -17,12 +17,27 @@ public class cameraControls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        // turn this off for testing game so you can move the cursor around when playing in unity
+            // turn back on when publishing game
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Get Input
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensVer;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensHor;
+        // Convert input to rotation float
+        xRotation += mouseY;
+        // clamp camera to rotation
+        xRotation = Mathf.Clamp(xRotation, lockVertMin, lockVertMax);
+
+        //rotate the camera on the X-Axis
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        //rotate the player on the Y-Axis
+        transform.parent.Rotate(Vector3.up * mouseX);
     }
 }
