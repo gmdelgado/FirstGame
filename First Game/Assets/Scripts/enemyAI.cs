5e5bbf4d50eb2f5752a337b0fc99884f24f5e30b
Gabriel Martinez Delgado <106568654+gmdelgado@users.noreply.github.com>
2023-04-28 19:01:09 -0600

    Update Enemy AI script
    to make damageable enemy we need to add ",IDamage" to the main function
    to remove the error from IDamage we need to add the function
    void takeDamage(int amount)
    we have to make it public and it has to be spelled the same as in the IDamage script.

--- First Game/Assets/Scripts/enemyAI.cs
+++ First Game/Assets/Scripts/enemyAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyAI : MonoBehaviour
+public class enemyAI : MonoBehaviour, IDamage
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,10 @@ public class enemyAI : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void takeDamage(int amount)
+    {
+
     }
 }
